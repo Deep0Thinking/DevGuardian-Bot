f58e189e2b31ff209fdfc26703f1c59a2adaaa2b
Louis Liu <Deep_Thinking@outlook.com>
2024-02-11 02:48:03 -0800

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@
 
 from datetime import timedelta
 
+
 # ID of the Discord channel to send reports to
 CHANNEL_ID = 
 
@@ -10,16 +11,25 @@ CHANNEL_ID =
 BOT_TOKEN = ''
 
 # List of predefined authors to track
-authors_list = ["",]
+AUTHORS_LIST = ["",]
 
-# Current style for report formatting: False for table-like, True for alternate style
-current_style = False 
+# Current style for report formatting; False for table-like, True for alternate style
+CURRENT_STYLE = False 
 
 # Time interval for periodic reports (weeks=? or days=? or hours=? or minutes=? or seconds=?)
-report_interval = timedelta(seconds=10)
+REPORT_INTERVAL = timedelta(seconds=10)
+
+# Minimum number of contributions to be considered for periodic reports
+MIN_PERIODIC_CONTRIBUTIONS = 
+
+# Maximum number of formal warnings before being expelled
+MAX_FORMAL_WARNINGS = 
 
 # Periodic contributions history file path
 PERIODIC_CONTRIBUTIONS_HISTORY_FILE = 'periodic_contributions_history.json'
 
 # Total contributions file path
 TOTAL_CONTRIBUTIONS_FILE = 'total_contributions.json'
+
+# Formal warnings file path
+FORMAL_WARNINGS_FILE = 'formal_warnings.json'
